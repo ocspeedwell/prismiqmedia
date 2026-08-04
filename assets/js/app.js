5b642c6c410fa55e63c472dcd2ae0ae6bf07138f
ocspeedwell <104182988+ocspeedwell@users.noreply.github.com>
2026-08-04 05:57:49 +0100

    Create app.js

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+const header = document.querySelector(".header");
+
+window.addEventListener("scroll",()=>{
+
+    if(window.scrollY>40){
+
+        header.classList.add("scrolled");
+
+    }else{
+
+        header.classList.remove("scrolled");
+
+    }
+
+});
