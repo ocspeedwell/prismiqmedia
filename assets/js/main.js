80a4d42f3b3451aafa4dfb921791127046a19191
ocspeedwell <104182988+ocspeedwell@users.noreply.github.com>
2026-08-04 05:22:34 +0100

    Create main.js

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+// Sticky Navigation
+
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 80){
+
+        navbar.classList.add("scrolled");
+
+    }else{
+
+        navbar.classList.remove("scrolled");
+
+    }
+
+});
