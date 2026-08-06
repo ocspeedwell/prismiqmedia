aea448d64bd512d9cf70bae7ca403dac480bc7c4
ocspeedwell <104182988+ocspeedwell@users.noreply.github.com>
2026-08-06 11:30:48 +0100

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,3 +7,58 @@ menuToggle.addEventListener("click",()=>{
 navMenu.classList.toggle("active");
 
 });
+/* ==========================================
+   COUNTER
+========================================== */
+
+const counters = document.querySelectorAll(".counter");
+
+const runCounter = () => {
+
+    counters.forEach(counter => {
+
+        const target = +counter.dataset.target;
+
+        const update = () => {
+
+            const current = +counter.innerText;
+
+            const increment = Math.ceil(target / 60);
+
+            if(current < target){
+
+                counter.innerText = current + increment;
+
+                setTimeout(update,25);
+
+            }else{
+
+                counter.innerText = target;
+
+            }
+
+        };
+
+        update();
+
+    });
+
+};
+
+const observer = new IntersectionObserver(entries => {
+
+    entries.forEach(entry => {
+
+        if(entry.isIntersecting){
+
+            runCounter();
+
+            observer.disconnect();
+
+        }
+
+    });
+
+});
+
+observer.observe(document.querySelector(".impact"));
